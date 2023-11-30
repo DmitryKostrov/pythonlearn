d8e29cd5cae584970652f056f3ccd1cd4b42cc55
DmitryKostrov <dimon3limon3@gmail.com>
2023-11-30 21:49:25 +0300

    Arithmetic Operation
    - Add folder arithmetic_operation
    - add class arithmetic_operation
      - function addition,subtraction,division,multiplication

---
+++ arithmetic_operations/main.py
@@ -0,0 +1,12 @@
+class arithmetic_operation():
+    def add_operation(a, b):
+        return a + b
+    def sub_operation(a, b):
+        return a - b
+    def divi_operation(a, b):
+        return a / b
+    def mult_operation(a, b):
+        return a * b
+
+if __name__ == '__main__':
+    print(arithmetic_operation.mult_operation(10,2))
